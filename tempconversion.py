5199b3ecb2aeb87b19c946a09ca8ecfe87ec60e5
chilukurinavyagoud <168103816+chilukurinavyagoud@users.noreply.github.com>
2024-12-02 14:00:17 +0530

    Add files via upload
    Conversion of temperature

---
+++ tempconversion.py
@@ -0,0 +1,11 @@
+#coversion of 
+unit = input("Is this temperature Celcius or fahrenhit?")
+temp = float(input("Enter the temperature:"))
+if unit == "Celcius":
+    temp = round((temp*9)/5+32)
+    print(f"the temperature in fahrenhit is: {temp}°F")
+elif unit == "fahrenhit":
+    temp = round((temp -32)*5/9,1)
+    print(f"the temperature in Celcius is:{temp}°C")
+else:
+    print(f"the {unit} is an invalid measurement")
